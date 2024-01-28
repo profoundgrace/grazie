448d8f58ae644bda130ff1b6a2e430b3e066b18b
David Dyess II <david.dyess@gmail.com>
2024-01-27 19:07:26 -0700

    Added Dashboard page template

--- app/themes/favorite/index.tsx
+++ app/themes/favorite/index.tsx
@@ -1,5 +1,9 @@
-import { Box } from '@mantine/core';
+import { Anchor, AppShell, Box, Burger, Group, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { Link } from '@remix-run/react';
 import type { ReactNode } from 'react';
+import Navbar from '~/components/Dashboard/Navbar';
+import { site } from '@/grazie';
 
 import { Footer } from '~/themes/favorite/components/Footer';
 import { Header } from '~/themes/favorite/components/Header';
@@ -14,8 +18,44 @@ export function Page({ children }: { children: ReactNode }) {
   );
 }
 
+export function Dashboard({ children }: { children: ReactNode }) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Anchor component={Link} to="/" underline="never">
+            <Text
+              gradient={{ from: 'indigo', to: 'blue', deg: 90 }}
+              fw={500}
+              size="xl"
+              variant="gradient"
+            >
+              {site?.name ?? 'Site Name'}
+            </Text>
+          </Anchor>
+          <Text fw={500} size="xl">
+            Dashboard
+          </Text>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar>
+        <Navbar />
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
+
 export const Theme = {
-  Page
+  Page,
+  Dashboard
 };
 
 export default Page;
