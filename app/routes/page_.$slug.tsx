e8bb29636519f0d6bcd5419c01f40672770319a3
David Dyess II <david.dyess@gmail.com>
2024-01-27 19:05:58 -0700

    Added Pages

---
+++ app/routes/page_.$slug.tsx
@@ -0,0 +1,48 @@
+import { Title, Grid, Tabs } from '@mantine/core';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData, useNavigate } from '@remix-run/react';
+import type { Page } from '~/types/Page';
+import PageCard from '~/components/Page/PageCard';
+import { getPage } from '~/lib/page.server';
+import { site } from '@/grazie';
+
+export function meta({
+  data: {
+    page: { title }
+  }
+}) {
+  return [{ title: `${title}${site?.separator}${site?.name}` }];
+}
+
+export async function loader({ params, request }: LoaderFunctionArgs) {
+  const page = await getPage({ slug: params?.slug });
+
+  const data = { page };
+  return json(data);
+}
+
+export default function Page() {
+  const data = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+  const { page } = data;
+
+  return (
+    <Grid>
+      <Grid.Col span={12}>
+        <PageCard
+          key={page.id}
+          data={{
+            ...page,
+            body: JSON.parse(page.body),
+            author: {
+              name: page?.author?.displayName,
+              description: '',
+              image: ''
+            }
+          }}
+        />
+      </Grid.Col>
+    </Grid>
+  );
+}
---
+++ app/routes/pages.tsx
@@ -0,0 +1,64 @@
+import { Title, Grid, Tabs } from '@mantine/core';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData, useNavigate } from '@remix-run/react';
+import type { Page } from '~/types/Page';
+import PageCard from '~/components/Page/PageCard';
+import { getPages } from '~/lib/page.server';
+import { site } from '@/grazie';
+
+export function meta() {
+  return [{ title: `Pages${site?.separator}${site?.name}` }];
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const pages = await getPages({});
+
+  const data = { pages };
+
+  return json(data);
+}
+
+export default function Articles() {
+  const data = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+  const pages =
+    data?.pages?.nodes?.length > 0 ? (
+      data?.pages?.nodes?.map((article) => (
+        <PageCard
+          key={article.id}
+          data={{
+            ...article,
+            body: JSON.parse(article.body),
+            category: null,
+            author: {
+              name: article?.author?.displayName,
+              description: '',
+              image: ''
+            }
+          }}
+        />
+      ))
+    ) : (
+      <h4>No Pages Stored</h4>
+    );
+
+  return (
+    <Grid>
+      <Grid.Col span={12}>
+        <Title order={2}>Pages</Title>
+        <Tabs defaultValue="browse" keepMounted={false}>
+          <Tabs.List>
+            <Tabs.Tab value="create" onClick={() => navigate('/page/create')}>
+              Create
+            </Tabs.Tab>
+            <Tabs.Tab value="browse">Browse</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel value="browse" py={10}>
+            {pages}
+          </Tabs.Panel>
+        </Tabs>
+      </Grid.Col>
+    </Grid>
+  );
+}
