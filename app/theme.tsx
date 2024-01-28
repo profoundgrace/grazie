efabdca0f237d3e5577f0195cbb06b59aab69a9b
David Dyess II <david.dyess@gmail.com>
2024-01-27 19:07:50 -0700

    Added dynamic page overrides via config

--- app/theme.tsx
+++ app/theme.tsx
@@ -1,19 +1,24 @@
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { ThemeContext } from '~/hooks/useTheme';
 import { theme, page, Theme, Pages } from '@/grazie';
+import { useMatches, useRouteLoaderData } from '@remix-run/react';
+import useMatchesData from './hooks/useMatchesData';
 
-export function ThemeProvider({
-  children,
-  defaultTheme = theme,
-  defaultPage = page
-}) {
-  const [theme, setTheme] = useState(defaultTheme);
-  const [page, setPage] = useState(defaultPage);
-  const Page =
-    page !== defaultPage ? Theme[page] : Theme[Pages?.root ?? defaultPage];
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const matches = useMatches();
+  const { id } = matches[matches.length - 1];
+  const loader = useRouteLoaderData(id);
+  const [_theme, setTheme] = useState(theme);
+  const [_page, setPage] = useState('root');
+  console.log(loader?._page);
+  if (loader && loader?._page !== _page) {
+    setPage(loader._page);
+  }
+  const Page = Theme?.[Pages?.[_page] ?? Theme?.[Pages?.[_page]] ?? page];
+  const { theme: data } = useMatchesData('root');
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme, page, setPage }}>
+    <ThemeContext.Provider value={{ theme, setTheme, page, setPage, data }}>
       <Page>{children}</Page>
     </ThemeContext.Provider>
   );
