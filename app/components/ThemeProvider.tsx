074713d240f7cbd7eb16fc2a3c337933dc90ec37
David Dyess II <david.dyess@gmail.com>
2024-02-01 08:39:02 -0700

    Moved ThemeProvider to components folder

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -1,8 +1,8 @@
+import { useMatches, useRouteLoaderData } from '@remix-run/react';
 import { ReactNode, useState } from 'react';
 import { ThemeContext } from '~/hooks/useTheme';
+import useMatchesData from '~/hooks/useMatchesData';
 import { theme, page, Theme, Pages } from '@/grazie';
-import { useMatches, useRouteLoaderData } from '@remix-run/react';
-import useMatchesData from './hooks/useMatchesData';
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   const matches = useMatches();
@@ -10,7 +10,6 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
   const loader = useRouteLoaderData(id);
   const [_theme, setTheme] = useState(theme);
   const [_page, setPage] = useState('root');
-  console.log(loader?._page);
   if (loader && loader?._page !== _page) {
     setPage(loader._page);
   }
