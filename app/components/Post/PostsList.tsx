c5839becc6d428dd0504fd9c952a52da164f479a
David Dyess II <david.dyess@gmail.com>
2024-03-17 19:09:31 -0600

    Move Posts components to a single component for routes

---
+++ app/components/Post/PostsList.tsx
@@ -0,0 +1,55 @@
+import { Title, Grid, Tabs } from '@mantine/core';
+import { useLoaderData, useNavigate } from '@remix-run/react';
+import PostCard from '~/components/Post/PostCard';
+import Pager from '~/components/Pager/Pager';
+import { subject, useAbility } from '~/hooks/useAbility';
+import { loader } from '~/routes/posts';
+
+export default function Articles() {
+  const data = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+  const ability = useAbility();
+
+  const posts =
+    data?.posts?.nodes?.length > 0 ? (
+      data?.posts?.nodes?.map((post) => (
+        <PostCard
+          key={post.id}
+          data={{
+            ...post,
+            body: JSON.parse(post.body),
+            author: {
+              name: post?.author?.displayName,
+              description: '',
+              image: `${data?.posts?.avatarURL}sm/${post?.author?.avatar}`
+            }
+          }}
+        />
+      ))
+    ) : (
+      <h4>No Posts Stored</h4>
+    );
+
+  return (
+    <Grid>
+      <Grid.Col span={12}>
+        <Title order={2}>Posts</Title>
+        <Tabs defaultValue="browse" keepMounted={false}>
+          <Tabs.List>
+            {ability.can('create', subject('Post', {})) && (
+              <Tabs.Tab value="create" onClick={() => navigate('/post/create')}>
+                Create
+              </Tabs.Tab>
+            )}
+            <Tabs.Tab value="browse">Browse</Tabs.Tab>
+          </Tabs.List>
+          <Pager />
+          <Tabs.Panel value="browse" py={10}>
+            {posts}
+          </Tabs.Panel>
+          <Pager />
+        </Tabs>
+      </Grid.Col>
+    </Grid>
+  );
+}
--- app/routes/posts_.$category.tsx
+++ app/routes/posts_.$category.tsx
@@ -1,13 +1,9 @@
-import { Title, Grid, Tabs } from '@mantine/core';
 import type { LoaderFunctionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { useLoaderData, useNavigate } from '@remix-run/react';
-import type { Post } from '~/types/Post';
-import PostCard from '~/components/Post/PostCard';
 import { getPosts } from '~/lib/post.server';
 import { site } from '@/grazie';
 import { pagerParams } from '~/utils/searchParams.server';
-import Pager from '~/components/Pager/Pager';
+import PostsList from '~/components/Post/PostsList';
 
 export function meta() {
   return [{ title: `Posts${site?.separator}${site?.name}` }];
@@ -28,47 +24,4 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
   return json(data);
 }
 
-export default function PostsCategory() {
-  const data = useLoaderData<typeof loader>();
-  const navigate = useNavigate();
-  const posts =
-    data?.posts?.nodes?.length > 0 ? (
-      data?.posts?.nodes?.map((post) => (
-        <PostCard
-          key={post.id}
-          data={{
-            ...post,
-            body: JSON.parse(post.body),
-            author: {
-              name: post?.author?.displayName,
-              description: '',
-              image: `${data?.posts?.avatarURL}sm/${post?.author?.avatar}`
-            }
-          }}
-        />
-      ))
-    ) : (
-      <h4>No Posts Stored</h4>
-    );
-
-  return (
-    <Grid>
-      <Grid.Col span={12}>
-        <Title order={2}>Posts</Title>
-        <Tabs defaultValue="browse" keepMounted={false}>
-          <Tabs.List>
-            <Tabs.Tab value="create" onClick={() => navigate('/post/create')}>
-              Create
-            </Tabs.Tab>
-            <Tabs.Tab value="browse">Browse</Tabs.Tab>
-          </Tabs.List>
-          <Pager />
-          <Tabs.Panel value="browse" py={10}>
-            {posts}
-          </Tabs.Panel>
-          <Pager />
-        </Tabs>
-      </Grid.Col>
-    </Grid>
-  );
-}
+export default PostsList;
