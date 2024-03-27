a12b7c99a8334a4653370e66f34a0b2383e1ba08
David Dyess II <david.dyess@gmail.com>
2024-03-27 15:54:15 -0600

    Removed dev prisma folder

--- prisma/migrations/20240201164626_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Setting" ADD COLUMN "description" TEXT;
-
--- CreateIndex
-CREATE INDEX "idx_setting_description" ON "Setting"("description");
--- prisma/migrations/20240225185616_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Note" ADD COLUMN "title" TEXT;
-
--- CreateIndex
-CREATE INDEX "idx_note_title" ON "Note"("title");
