bd3fc68d69566c08d9e0832d55cde9b6a674f4e5
David Dyess II <david.dyess@gmail.com>
2024-03-29 15:09:14 -0600

    Updated Sentry library to check a single item or an array of items; updated object prop to item (object); added items (array or undefined) prop

--- app/lib/sentry.server.ts
+++ app/lib/sentry.server.ts
@@ -1,15 +1,35 @@
 import { subject as Subject } from '@casl/ability';
 import { status } from './error.server';
+/**
+ * Sentry Access Control
+ * @param request
+ * @param param1
+ * @param param2
+ * @returns boolean | http status 404
+ */
 export async function sentry(
   request: Request,
   {
     action,
     subject,
-    object = {}
-  }: { action: string; subject: string; object?: object },
+    item = {},
+    items
+  }: { action: string; subject: string; item?: object; items?: [] },
   { reject = true } = {}
 ) {
-  if (!request.ability.can(action, Subject(subject, object))) {
+  if (Array.isArray(items)) {
+    for (const item of items) {
+      if (!request.ability.can(action, Subject(subject, item))) {
+        if (reject) {
+          return status(404);
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    }
+  } else if (!request.ability.can(action, Subject(subject, item))) {
     if (reject) {
       return status(404);
     } else {
