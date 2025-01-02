d11093502286d0ece106600c20bed8c475d71ee1
David Dyess II <david.dyess@gmail.com>
2025-01-02 11:31:48 -0700

    Added Rock theme - silver color scheme

---
+++ app/themes/rock/components/Footer.tsx
@@ -0,0 +1,40 @@
+import { Group, ActionIcon, rem, Text } from '@mantine/core';
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram
+} from '@tabler/icons-react';
+import { site as defaultSite } from '@/grazie';
+import classes from '~/themes/rock/styles/Footer.module.css';
+import { FooterLinks } from '~/components/FooterLinks';
+import { SocialIcons } from '~/components/SocialIcons';
+import { useTheme } from '~/hooks/useTheme';
+import { ColorSchemeToggle } from '~/components/ColorScheme/ColorSchemeToggle';
+
+export function Footer() {
+  const {
+    data: { site }
+  } = useTheme();
+  return (
+    <div className={classes.footer}>
+      <div className={classes.inner}>
+        <Text fw={500} size="md">
+          {site?.name ?? defaultSite?.name ?? 'Site Name'}
+          <Text size="xs" color="dimmed">
+            Copyright &copy;{' '}
+            {site?.copyright ?? defaultSite?.copyright ?? 'Copyright'}
+          </Text>
+        </Text>
+        <Group className={classes.links}>
+          <FooterLinks />
+        </Group>
+        <Group gap="xs" justify="flex-end" wrap="nowrap">
+          <SocialIcons />{' '}
+          <ColorSchemeToggle duration={2000} fadeOutDuration={6000} />
+        </Group>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ app/themes/rock/pages/default.tsx
@@ -0,0 +1,1 @@
+export { Page } from '~/themes/rock/pages/Page';
